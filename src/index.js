85f07d5a9245a5d5701a29e1ff18843e9abb57e8
Kirillsp1982 <kirillsp1982@gmail.com>
2020-03-03 13:48:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = [];
+	const result = [];
+    for (let i = 0; i < expr.length; i += 2) {
+		if (expr[i] === '0' && expr[i + 1] === '0') arr.push('+');
+		if (expr[i] === '1' && expr[i + 1] === '0') arr.push('.');
+		if (expr[i] === '1' && expr[i + 1] === '1') arr.push('-');
+		if (expr[i] === '*' && expr[i + 1] === '*') arr.push('*');
+	}
+	
+	for (let j = 0; j < arr.length; j += 5) {
+		let word = arr.slice(j, j + 5);			
+        word = word.filter(x => x !== '+').join('');
+        if (word === '*****') result.push(' ');		
+		result.push(MORSE_TABLE[word]);
+	}
+	return result.join('');
 }
 
 module.exports = {
